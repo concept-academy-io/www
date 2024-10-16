206255f5223749164d8f7b5868ceb74d80bdef2f
Tarek Adam <tarek.adam@gmail.com>
2024-10-16 14:23:21 -0600

    Upgrades rendering of index page menu

---
+++ course_navigation/render.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('/course_navigation/index.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(html => {
+            console.log(html);
+            var elements = document.getElementsByClassName('curriculum-menu');
+            console.log(elements.length);
+
+            for (var i = 0; i < elements.length; i++) {
+                var element = elements[i];
+                var content = html.replace(/{{.*?}}/g, '');
+                element.innerHTML = content;
+            }
+
+            // Reinitialize Bootstrap dropdowns
+            var dropdownElements = [].slice.call(document.querySelectorAll('.dropdown-toggle'));
+            dropdownElements.forEach(function (dropdownToggle) {
+                new bootstrap.Dropdown(dropdownToggle);
+            });
+
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+});
